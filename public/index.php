18d591d823e1a7c260a1a9e73aec7f773d8aef57
curlyRoss87 <leachr87@gmail.com>
2026-01-25 16:09:17 +0000

    Logo name in splash

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ $pageTitle = "GhostStack™";
   <main class="splash-wrap">
     <a class="splash-logo" href="/home.php" aria-label="Enter GhostStack™">
       <!-- Replace with your real SVG file -->
-      <img src="/assets/img/ghoststack-logo.svg" alt="GhostStack™ logo">
+      <img src="/assets/img/ghoststack_logo.svg" alt="GhostStack™ logo">
       <div class="splash-hint">Enter</div>
     </a>
 
